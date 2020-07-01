ffc3cbd5c78d45dbea38e47dc7eacf810c58f552
nandakumarpurohit <nandakumarpurohit@gmail.com>
2020-07-01 10:37:33 -0400

    Corrected syntax (Line #17)

--- src/main/java/com/demo/CaseConverter.java
+++ src/main/java/com/demo/CaseConverter.java
@@ -14,7 +14,7 @@ public class CaseConverter {
 			return data;
 		else {
 			for(int i=0;i<data.length();i++) {
-				int x = data.charAt(i)
+				int x = data.charAt(i);
 				x -= 32;
 				temp = temp + (char) x;
 			}
