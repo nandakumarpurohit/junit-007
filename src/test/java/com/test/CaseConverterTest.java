71c863d192c618f995982fd7bfc6b74cf3ae3949
nandakumarpurohit <nandakumarpurohit@gmail.com>
2021-07-20 12:31:20 -0400

    Fixed error

--- src/test/java/com/test/CaseConverterTest.java
+++ src/test/java/com/test/CaseConverterTest.java
@@ -12,7 +12,7 @@ public class CaseConverterTest {
 
 	@Test
 	public void testConvertCase() {
-		assertEquals(c1.converCase("hello"), "HELLO");
+		assertEquals(c1.convertCase("hello"), "HELLO");
 	}
 	
 	@Test
